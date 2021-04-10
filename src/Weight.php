f03eec9fcd09db85af30556614fe8b7bcd7385d2
keshav <keshav.istart@gmail.com>
2021-04-10 21:44:31 +0530

    convert Kilogram to lbs

---
+++ src/Weight.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Keshavistart\UnitConversions;
+
+class Weight
+{
+    private $kilogram;
+
+    public static function fromKilograms(float $kilogram)
+    {
+        return new static($kilogram);
+    }
+
+    public function __construct(float $kilogram) {
+        $this->kilogram = $kilogram;
+    }
+
+    public function toLbs(): float
+    {
+        return $this->kilogram*2.204623;
+    }
+}
---
+++ tests/WeightTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Keshavistart\UnitConversions\Tests;
+
+use Keshavistart\UnitConversions\Weight;
+use PHPUnit\Framework\TestCase;
+
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function it_can_convert_kilogram_to_lbs()
+    {
+        $lbs = Weight::fromKilograms(100)->toLbs();
+        $this->assertEquals(220.4623,$lbs);
+    }
+}
